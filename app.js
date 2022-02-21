da665a150744c604a1823dac0906e575ba8c5e1b
Joaquim Verges <joaquim.verges@gmail.com>
2022-02-21 11:02:56 -0800

    add couple of todos

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// map from address to twitter handle
+
+// SDK event listener
+// listens on transfer:
+// - increments transfer count
+// - calls twitter bot to mention with infor from transfer event
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/import", (req, res) => {
+  // TODO read body
+  // TODO write to disk
 });
 
 app.listen(port, () => {
