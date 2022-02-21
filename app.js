9c6ca92700aeb4045f86a17e4d2e445d55a82d10
ayush <62694274+ayshptk@users.noreply.github.com>
2022-02-22 01:03:56 +0530

    add twitter stubs

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const server = require(".");
+const { TwitterApi } = require("twitter-api-v2");
+var cron = require("node-cron");
+const fs = require("fs");
 
-// map from address to twitter handle
+const client = new TwitterApi(process.env.TWITTER_BEARER);
+const dbpath = "./data/pool.json";
 
-// SDK event listener
-// listens on transfer:
-// - increments transfer count
-// - calls twitter bot to mention with infor from transfer event
+let db = {};
+if (!fs.existsSync(dbpath)) {
+  fs.writeFileSync(dbpath, JSON.stringify({}));
+}
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const database = () => {
+  return JSON.parse(fs.readFileSync(dbpath));
+};
+
+// add new wallet to the address pool
+const addWallet = (address, username) => {
+  fs.writeFileSync(
+    dbpath,
+    JSON.stringify({ ...database(), [address]: username })
+  );
+};
+
+// run server
+server();
+
+async function tweetTransfer(address) {
+  const username = database()[address];
+  try {
+    await client.v1.tweet(
+      `The 🔥🥔 NFT has been transferred to ${
+        username ? "@" + username : address
+      }\n\nYou have 24 hours to transfer 🔥🥔 NFT to another address.\n\nJoin the Hot Potato NFT game: ${landing}`
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// check every minute for new replies
+cron.schedule("* * * * *", async () => {
+  try {
+    const query = await client.search("(to:hotpotatogg)");
+    console.log(query);
+    // TODO: add new addresses to pool
+    // addWallet(query..., query....);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.post("/import", (req, res) => {
-  // TODO read body
-  // TODO write to disk
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = {
+  tweetTransfer,
+  database,
+  addWallet,
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { tweetTransfer } = require("./app");
+const app = express();
+const port = 3000;
+
+// map from address to twitter handle
+
+// SDK event listener
+// listens on transfer:
+// - increments transfer count
+// - calls tweetTransfer() from transfer event
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/import", (req, res) => {
+  // TODO read body
+  // TODO write to disk
+});
+
+function run() {
+  server.listen(3000, () => {
+    console.log("server running on 3000");
+  });
+}
+
+module.exports = run;
